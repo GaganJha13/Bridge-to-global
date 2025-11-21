024563ab25534eb941b1ff5a693fefe435fb6b66
Gagan <gjha20367@gmail.com>
2025-11-21 21:38:02 +0530

    Fresh

--- bridgetoglobal/settings.py
+++ bridgetoglobal/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -118,12 +121,11 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / "static",    # keep your static folder here
+    os.path.join(BASE_DIR, 'static'),   # <-- put your images here: project_root/static/img/...
 ]
 
-# add for production/collectstatic
-STATIC_ROOT = BASE_DIR / "staticfiles"
-
+# For production (collectstatic target)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
